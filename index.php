bc0d234840d383d8e705864f7b0c3c2c28fc3a6f
Agung Dwi Sahputra <91104004+AgungDwiSahputra@users.noreply.github.com>
2022-06-14 15:34:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -103,6 +103,8 @@ if (isset($_POST['mulai'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>DENCRYPT | With Agung & Salma</title>
+    <link rel="shortcut icon" href="https://agungdwisahputra.github.io/Ucapan-Ulang-Tahun-Salma/img/galeri/alma%20(3).jpg" type="image/x-icon">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://agungdwisahputra.github.io/Ucapan-Ulang-Tahun-Salma/style.css">
 
